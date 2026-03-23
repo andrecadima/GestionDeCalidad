af77fe03365f1e91ef78d475557ea1adf76926cc
Andre Cadima <andrecadimaschool@gmail.com>
2026-03-23 10:26:34 -0400

    idk i gues we need it

--- MicroServices/MicroServicioUsuario.API/tests.bat
+++ MicroServices/MicroServicioUsuario.API/tests.bat
@@ -3,7 +3,7 @@
 dotnet sonarscanner begin ^
 /k:"mi_proyecto" ^
 /d:sonar.host.url="http://localhost:9000" ^
-/d:sonar.token="sqa_daee41845a8d83be4a4fce1654deccdc19e19c55" ^
+/d:sonar.token="sqa_57d254a558e41314c5db5c1431e5c6ef8c8348fd" ^
 /d:sonar.cs.opencover.reportsPaths="MicroServicioUser.Tests/TestResults/coverage/coverage.opencover.xml"
 
 dotnet build MicroServicioUsuario.API.sln
@@ -13,4 +13,4 @@ dotnet test MicroServicioUser.Tests/MicroServicioUser.Tests.csproj ^
 /p:CoverletOutput=./TestResults/coverage/ ^
 /p:CoverletOutputFormat=opencover
 
-dotnet sonarscanner end /d:sonar.token="sqa_daee41845a8d83be4a4fce1654deccdc19e19c55
+dotnet sonarscanner end /d:sonar.token="sqa_57d254a558e41314c5db5c1431e5c6ef8c8348fd"
